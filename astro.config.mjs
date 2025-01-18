b23d2bdc5f4db1bace001d2ce21cd4b920f4926f
Eliezer A Meza <150059983+EliezerMezaDev@users.noreply.github.com>
2025-01-17 20:22:17 -0400

    fix: add react compiler options

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,10 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react({
+      include: ["**/react/*"],
+      experimentalReactChildren: true,
+    }),
+  ],
 });
